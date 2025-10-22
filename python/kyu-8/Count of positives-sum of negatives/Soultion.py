9ff1c75c1a0c445472467f32ea6090ee290f114c
dalia <daliaakram119@gmail.com>
2025-10-22 19:47:12 +0300

    python/kyu-8/Count of positives-sum of negatives/

--- python/kyu-8/Count of positives-sum of negatives/Soultion.py
+++ python/kyu-8/Count of positives-sum of negatives/Soultion.py
@@ -8,7 +8,8 @@ def count_positives_sum_negatives(arr):
     for num in arr:
         if num > 0:
             count += 1
+#or positive_num=sum(1 for x in arr if x>0)
 
         elif num < 0:
-            sum_num += num
+            sum_num += num # negative_nums=sum(x for x in arr if x <0)
     return [count, sum_num]
